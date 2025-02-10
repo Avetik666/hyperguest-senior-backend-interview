46997748794a0a347aa4164e434c33a2efaea0c6
Avetik Sarikyan <saravetik@gmail.com>
2025-02-10 20:08:45 +0400

    fix: update queue.ts to handle messages properly

--- Queue.ts
+++ Queue.ts
@@ -1,26 +1,46 @@
 import {  Message } from "./Database";
 
 export class Queue {
-    private messages: Message[]
+    private messages: Map<string, Message>
+    private workerProcessing: Map<string, number>
 
     constructor() {
-        this.messages = []
+        this.messages = new Map<string, Message>();
+        this.workerProcessing = new Map<string, number>();
     }
 
     Enqueue = (message: Message) => {
-        this.messages.push(message)
+        this.messages.set(message.id, message)
     }
 
     Dequeue = (workerId: number): Message | undefined => {
-        return this.messages.splice(0,1)[0]
+        const [messageId] = this.messages.keys();
+        if (this.workerProcessing.has(messageId)) {
+            return;
+        }
+        const message  = this.messages.get(messageId);
+        this.workerProcessing.set(messageId, workerId);
+        return message;
     }
 
     Confirm = (workerId: number, messageId: string) => {
+        const workerWithAccess = this.workerProcessing.get(messageId);
+        if (workerWithAccess !== workerId) {
+            console.error('Wrong worker trying to confirm');
+            return;
+        }
 
+        const message = this.messages.get(messageId);
+        if (!message) {
+            return;
+        }
+
+        this.messages.delete(messageId);
+        this.workerProcessing.delete(messageId);
     }
 
     Size = () => {
-        return this.messages.length
+        return this.messages.size;
     }
 }
 
